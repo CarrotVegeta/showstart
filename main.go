77e5c7f4e4171d5b569c110c87de30ce207b492d
CarrotVegeta <>
2022-12-14 15:29:45 +0800

    更新导入包标识

--- main.go
+++ main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"github.com/CarrotVegeta/showstart/api"
-	router2 "github.com/CarrotVegeta/showstart/api/router"
+	"github.com/CarrotVegeta/showstart/api/router"
 )
 
 func main() {
 	s := api.NewServer()
-	s.RegisterRouter(&router2.Activity{})
-	s.RegisterRouter(&router2.Ticket{})
-	s.RegisterRouter(&router2.OrderServer{})
+	s.RegisterRouter(&router.Activity{})
+	s.RegisterRouter(&router.Ticket{})
+	s.RegisterRouter(&router.OrderServer{})
 	s.Start()
 }
